5b9e6ddc9caffc1b6d3cd4f22a10441babe2f188
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-07-12 21:59:12 +0200

    Update all

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let dropDownActionMovies = document.getElementById("dropdown_action");
 dropDownActionMovies.addEventListener('click', () => {
     const category = 'action';
-    loadMoviesBycategory(category);
+    loadMoviesByCategory(category);
 });
+const category = 'action';
+loadMoviesByCategory(category);
 
-async function loadMoviesBycategory(category) {
+async function loadMoviesByCategory(category) {
     const apiUrl = `http://localhost:8000/api/v1/titles/?sort_by=-votes&genre=${category}&limit=6`;
     const reponse = await fetch(apiUrl);
     const data = await reponse.json();
-    injectMoviesIntoPage(data.movies);
+    console.log(data)
+    injectMoviesIntoPage(data.results);
 }
 
 function injectMoviesIntoPage(movies) {
@@ -18,8 +21,7 @@ function injectMoviesIntoPage(movies) {
     movies.forEach(movie => {
         const movieElement = document.createElement('div');
         movieElement.className = 'movie';
-        movieElement.innerHTML = `<img src=${movie.posterUrl} alt=${movie.title}><h3>${movie.title}</h3><p>${movie.description}</p><button onclick="showDetails(${movie.id})">Détails</button>";
+        movieElement.innerHTML = `<img src=${movie.image_url} alt=${movie.title}><h3>${movie.title}</h3><button onclick=showDetails(${movie.id})>Détails</button>`;
         container.appendChild(movieElement);
     });
 }
-
