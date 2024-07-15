93528e8aafc30a787fb613c6f2eff29df1de9212
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-07-15 23:13:52 +0200

    Update all

---
+++ displayBestMovie.js
@@ -0,0 +1,19 @@
+let displayBestMovie = document.getElementById("best_movie");
+fetchBestMovie()
+
+async function fetchBestMovie() {
+    const apiUrlMoviesorted = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
+    const firstResponse = await fetch(apiUrlMoviesorted);
+    const jsonFirstResponse = await firstResponse.json();
+    const firstMovie = jsonFirstResponse.results[0];
+    const movieElementForData = document.createElement('a');
+    movieElementForData.innerHTML = `<img src=${firstMovie.image_url} alt=${firstMovie.title} width="259" heigth="252"><h2>${firstMovie.title}</h2>`;
+    displayBestMovie.appendChild(movieElementForData);
+
+    const apiUrlDataBestMovie = `${firstMovie.url}`
+    const secondResponse = await fetch(apiUrlDataBestMovie);
+    const jsonSecondResponse = await secondResponse.json();
+    const movieElementForDescription = document.createElement('p');
+    movieElementForDescription.innerHTML = jsonSecondResponse.description;
+    displayBestMovie.appendChild(movieElementForDescription);
+}
