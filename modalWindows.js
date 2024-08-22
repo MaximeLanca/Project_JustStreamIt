80cef5de44e9de11faad232d8c3eb690f87db553
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-08-22 23:26:42 +0200

    Update all

--- modalWindows.js
+++ modalWindows.js
@@ -1,23 +1,49 @@
-let modal = document.getElementById("myModal");
-let modalButtons = document.getElementsByClassName("openModalBtn")
-/* let red_btn = document.getElementById("openModalBtn");
-let black_btn = document.getElementById("openModalBtn"); */
-let span = document.getElementsByClassName("close")[0];
+function getMovieInfoForModal(dataMovie) {
 
-for (let i = 0; i < modalButtons.length; i++) {
-    let button = modalButtons[i];
-    console.log(button)
-    console.log(modalButtons.length)
-    button.addEventListener("click", function () {
-        modal.style.display = "block";
-    })
-};
+    const movieElementForModal = document.getElementById('modal-content');
 
-span.onclick = function () {
-    modal.style.display = "none";
+    movieElementForModal.innerHTML =
+        `<h2 class="h2_modal_title_movie">${dataMovie.title}</h2>  
+         <div class="modal_information_movie_partOne">
+            <p>${dataMovie.year} - ${dataMovie.genres}</p>
+            <p>${dataMovie.duration} minutes - ${dataMovie.countries}</p>
+            <p>IMDB Score: ${dataMovie.imdb_score} / 10</p>
+            <p>Réaliser par: ${dataMovie.writers}</p>
+        </div>
+        <div class="modal_picture_movie">
+            <a>
+                <img src=${dataMovie.image_url} alt=${dataMovie.title} width="209" heigth="202">
+            </a>
+        </div>
+        <div class="modal_information_movie_partTwo">
+            <p>${dataMovie.long_description}</p>
+            <p>Avec:<br>${dataMovie.actors}</p>
+        </div>
+        <button id="closeModalButton" class="closeModalButton">Fermer</button>
+        `;
+
+    const closeModalBtn = document.getElementById('closeModalButton');
+    closeModalBtn.addEventListener('click', function () {
+        document.getElementById('myModal').style.display = 'none';
+        document.getElementById('modalOverlay').style.display = 'none';
+    });
+
 }
 
-console.log(modalButtons)
-async function modalContent() {
+document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModalButton');
+    const modal = document.getElementById('myModal');
+    const modalOverlay = document.getElementById('modalOverlay');
 
-}
+    openModalBtn.addEventListener('click', function () {
+        modalOverlay.style.display = 'block'; modal.style.display = 'block';
+
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+            modalOverlay.style.display = 'none';
+        }
+    });
+});
