ceb4032b3031354c8bbedfb399bc51db6ea951d8
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-10-14 22:08:09 +0200

    Update button dropdown

--- displayMultiCategoryMoviesWithDropDown.js
+++ displayMultiCategoryMoviesWithDropDown.js
@@ -19,24 +19,48 @@ async function fetchGenresMovies() {
         page++;
     };
 
+
     let dropDownContent = document.getElementById("dropdownContent");
     for (let category of moviesCategoriesList) {
-        dropDownContent.innerHTML += `<a id="${category.toLowerCase()}" class="relative inline-block"><p>${category}</p></a>`;
+        dropDownContent.innerHTML += `
+        <div id="${category.toLowerCase()}" class="text-2xl border border-black"> 
+            <a class="w-[150px]">
+                <div class="pl-2 relative">
+                    <p>${category}</p>
+                    <span class="absolute right-0 top-0.5 icon float-right w-[25px] h-[25px]"></span>
+                </div>
+            </a>
+        </div>`;
     };
 
     attachDropdownEvents();
 };
 
 function attachDropdownEvents() {
+    let selectedCategory = null;
     for (let category of moviesCategoriesList) {
+
         const button = document.getElementById(`${category.toLowerCase()}`);
+
         button.addEventListener('click', function () {
             const moviesContainer = document.getElementById("moviesContainerDropDown");
             moviesContainer.innerHTML = "";
             fetchMovies("moviesContainerDropDown", `sort_by=-imdb_score&genre=${category}`);
+
+            // Vérifie si une catégorie est déjà sélectionnée
+            if (selectedCategory) {
+                // Supprime les styles de l'icône de la catégorie précédemment sélectionnée
+                selectedCategory.querySelector('.icon').classList.remove('bg-green-500', 'bg-cover', 'border', 'border-green-400', 'rounded-md', 'text-white', 'flex', 'items-center', 'justify-center');
+                selectedCategory.querySelector('.icon').innerHTML = ''; // Vide l'icône
+            }
+
+            // Sélectionne la nouvelle catégorie
+            selectedCategory = button; // Met à jour la catégorie sélectionnée
+            button.querySelector('.icon').classList.add('bg-green-500', 'bg-cover', 'border', 'border-green-400', 'rounded-md', 'text-white', 'flex', 'items-center', 'justify-center');
+            button.querySelector('.icon').innerHTML = '&#10003;';  // Ajoute la coche verte
         });
     };
-};
+}
 
 document.getElementById("dropBtn").addEventListener("click", function () {
     const dropdownContent = document.getElementById("dropdownContent");
@@ -44,3 +68,4 @@ document.getElementById("dropBtn").addEventListener("click", function () {
 });
 
 fetchGenresMovies();
+
